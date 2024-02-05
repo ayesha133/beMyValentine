e1d4c257be26d66075ce680551ef0f9ea96a432f
ayesha133 <ayesharshad88@gmail.com>
2024-02-05 18:27:22 -0500

    Updates

---
+++ precache-manifest.12992b810efdbc7b0277ac044088490b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e741f0b57a8d55d866c155ed14288f8",
+    "url": "/beMyValentine/index.html"
+  },
+  {
+    "revision": "5646b9b459d63ea82f41",
+    "url": "/beMyValentine/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "bce171e49cdcb964940a",
+    "url": "/beMyValentine/static/css/main.a878c9b8.chunk.css"
+  },
+  {
+    "revision": "5646b9b459d63ea82f41",
+    "url": "/beMyValentine/static/js/2.ce455bf1.chunk.js"
+  },
+  {
+    "revision": "5099652deffd979d970e",
+    "url": "/beMyValentine/static/js/3.d5f86460.chunk.js"
+  },
+  {
+    "revision": "bce171e49cdcb964940a",
+    "url": "/beMyValentine/static/js/main.30c99d0c.chunk.js"
+  },
+  {
+    "revision": "58b583bfaa369871b390",
+    "url": "/beMyValentine/static/js/runtime~main.cc5cb266.js"
+  },
+  {
+    "revision": "dbe129e6d2e91948606957a82f9c9681",
+    "url": "/beMyValentine/static/media/flowerCouch.dbe129e6.jpg"
+  },
+  {
+    "revision": "8d1fcd034b0fb5a34b24f906c0f6343e",
+    "url": "/beMyValentine/static/media/stickMan.8d1fcd03.png"
+  },
+  {
+    "revision": "9655eec17794428b85ce522566200ca6",
+    "url": "/beMyValentine/static/media/twoBear.9655eec1.gif"
+  }
+]);
